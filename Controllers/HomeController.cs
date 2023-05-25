bf2ce52017aabbe134c9a87c116d651db2c7abce
utsav <utsavadhikari452@gmail.com>
2023-05-25 08:56:09 +0545

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+       var list = new List<HomeVm>() {
+       new HomeVm() {Id =1, Name ="One", Address ="Ktm"},
+       new HomeVm() {Id =2, Name ="Two", Address ="Btm"}, 
+    };
+       return View(list);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
